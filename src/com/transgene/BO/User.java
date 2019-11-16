2a234c282c5885ab768cdcc2917cb0440af1d976
divysri <divyatransgene@gmail.com>
2019-11-16 21:55:42 +0530

    this is first full project push to repository

---
+++ src/com/transgene/BO/User.java
@@ -0,0 +1,11 @@
+package com.transgene.BO;
+
+public class User {
+
+	public static void main(String[] args) {
+		
+		System.out.println("helllo Testing DAO---->User");
+	
+	}
+
+}
---
+++ src/com/transgene/DAO/UserDAO.java
@@ -0,0 +1,10 @@
+package com.transgene.DAO;
+
+public class UserDAO {
+
+	public static void main(String[] args) {
+		
+		System.out.println("helllo Testing///// DAO");
+	}
+
+}
