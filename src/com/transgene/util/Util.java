dc0a0c5a9fdda58cdcbd5a614a5c525546d74328
divysri <divyatransgene@gmail.com>
2019-11-17 18:51:16 +0530

    util got changed

--- src/com/transgene/util/Util.java
+++ src/com/transgene/util/Util.java
@@ -5,4 +5,10 @@ public class Util {
 	public static void main(String args[]){
 	System.out.println("hellllo... hiiiii utility");
 }
+	
+	
+	public void display()
+	{
+		System.out.println("helllloo... hiiiiii");
+	}
 }
